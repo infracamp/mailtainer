954d04781791d6d1fcac29505683e4bb5fe0d984
Matthes <matthes@leuffen.de>
2022-01-04 10:45:04 +0100

    update

--- scripts/letsencrypt.sh
+++ scripts/letsencrypt.sh
@@ -4,5 +4,5 @@ set -ex
 
 if [[ "$ENABLE_LETSENCRYPT" == "1" ]]; then
     echo "Letsencrypt enabled - trying to load cert for $MAILNAME..."
-    certbot certonly -n --config-dir /data/letsencrypt --agree-tos -m admin@$MAILNAME --webroot -w /var/www/html -d $MAILNAME
+    certbot certonly -n --config-dir /data/letsencrypt --agree-tos -m admin@$MAILNAME --webroot -w /opt/www/ -d $MAILNAME
 fi;
